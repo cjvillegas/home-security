f582c7543777776d6ba486e5a815aff930096a0a
Chaprel John Villegas <chaprel.villegas@stylebyglobal.com>
2021-07-19 22:37:58 +0800

    :factory: reports optimization

--- routes/web/reports.php
+++ routes/web/reports.php
@@ -1,8 +1,7 @@
 <?php
 Route::group(['prefix' => 'reports', 'as' => 'reports.', 'namespace' => 'Report'], function () {
     // work analytics
-    Route::get('/work-analytics/get-hourly-analytics', 'WorkAnalyticsReportController@getHourlyAnalytics')->name('work-analytics.get-hourly-analytics');
-    Route::get('/work-analytics/get-daily-analytics', 'WorkAnalyticsReportController@getDailyAnalytics')->name('work-analytics.get-daily-analytics');
+    Route::get('/work-analytics/get-work-analytics', 'WorkAnalyticsReportController@getWorkAnalytics')->name('work-analytics.get-work-analytics');
     Route::get('/work-analytics/manufactured-blinds-analytics', 'WorkAnalyticsReportController@manufacturedBlindsAnalytics')->name('home');
     Route::resource('work-analytics', 'WorkAnalyticsReportController');
 
