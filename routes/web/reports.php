6af8438a98066848645eb8bdde9bab4e6d525e3b
Chaprel John Villegas <chaprel.villegas@stylebyglobal.com>
2021-07-26 19:49:36 +0800

    :wrench: enclosed work-analytics route in a route group

--- routes/web/reports.php
+++ routes/web/reports.php
@@ -1,10 +1,13 @@
 <?php
 Route::group(['prefix' => 'reports', 'as' => 'reports.', 'namespace' => 'Report'], function () {
     // work analytics
-    Route::get('/work-analytics/get-work-analytics', 'WorkAnalyticsReportController@getWorkAnalytics')->name('work-analytics.get-work-analytics');
-    Route::get('/work-analytics/manufactured-blinds-analytics', 'WorkAnalyticsReportController@manufacturedBlindsAnalytics')->name('work-analytics.manufactured-blinds-analytics');
-    Route::get('/work-analytics/despatch-department-analytics', 'WorkAnalyticsReportController@despatchDepartmentAnalytics')->name('work-analytics.despatch-department-analytics');
-    Route::resource('work-analytics', 'WorkAnalyticsReportController')->only(['index']);
+    Route::group(['prefix' => 'work-analytics', 'as' => 'work-analytics.'], function () {
+        Route::get('/get-work-analytics', 'WorkAnalyticsReportController@getWorkAnalytics')->name('get-work-analytics');
+        Route::get('/manufactured-blinds-analytics', 'WorkAnalyticsReportController@manufacturedBlindsAnalytics')->name('manufactured-blinds-analytics');
+        Route::get('/despatch-department-analytics', 'WorkAnalyticsReportController@despatchDepartmentAnalytics')->name('despatch-department-analytics');
+        Route::resource('', 'WorkAnalyticsReportController')->only(['index']);
+    });
+
 
     Route::get('/data-export', 'ReportController@dataExport')->name('data-export.index');
 });
