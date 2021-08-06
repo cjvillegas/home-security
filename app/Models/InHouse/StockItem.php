5b38a9d964ad7062b00d759a8ca4a4f826de8bf8
Louise Virtudazo <louise.virtudazo@iserviceglobalbiz.com>
2021-08-06 19:59:09 +0800

    SoftDelete

--- app/Models/InHouse/StockItem.php
+++ app/Models/InHouse/StockItem.php
@@ -4,9 +4,11 @@ namespace App\Models\InHouse;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class StockItem extends Model
 {
+    use SoftDeletes;
     use HasFactory;
 
     protected $guarded = [];
