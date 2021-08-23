83b3320d04ac41130a316431578933e987c4a345
Chaprel John Villegas <chaprel.villegas@stylebyglobal.com>
2021-08-23 22:19:55 +0800

    updated orders cron

--- app/Console/Commands/Cron/PopulateOrdersFromBlindData.php
+++ app/Console/Commands/Cron/PopulateOrdersFromBlindData.php
@@ -97,7 +97,8 @@ class PopulateOrdersFromBlindData extends Command
     {
         $latestSerialId = Order::getLatestSerialId();
 
-        $newQuery = "
+        // initialize the query
+        $query = "
             SELECT
                 sdl.id AS SerialID,
                 o.order_id AS OrderNo,
@@ -137,47 +138,10 @@ class PopulateOrdersFromBlindData extends Command
                 AND (bt.id <> '382')
         ";
 
-        // initialize the query
-        $query = "
-            SELECT
-                TOP 1000
-                OrderDetail.id AS BlindId,
-                [Order].order_id AS OrderNo,
-                [User].company AS Customer,
-                [Order].cust_no AS CustOrdNo,
-                OrderDetail.quantity AS Quantity,
-                BlindType.code AS BlindCode,
-                DetailStatus.name AS BlindStatus,
-                [Order].dat_delivery AS DespatchDate,
-                [Order].dat_order AS Ordered,
-                [Order].username AS OrderEnteredBy,
-                SerialDetailLine.id AS SerialID,
-                [Category].name AS CategoryName,
-                [Category].id AS CategoryID
-            FROM
-                OrderDetail
-                INNER JOIN [Order] ON OrderDetail.order_id = [Order].id
-                INNER JOIN [User] ON [Order].user_id = [User].id
-                INNER JOIN BlindType ON OrderDetail.blindtype_id = BlindType.id
-                INNER JOIN Fabric ON OrderDetail.fabric_id = Fabric.id
-                INNER JOIN OrderStatus ON [Order].orderstatus_id = OrderStatus.id
-                INNER JOIN DetailStatus ON OrderDetail.detailstatus_id = DetailStatus.id
-                INNER JOIN ManLocation ON BlindType.manlocation_id = ManLocation.id
-                INNER JOIN SerialDetailLine ON OrderDetail.id = SerialDetailLine.OrderDetail_id
-                INNER JOIN [Category] ON BlindType.category_id = [Category].id
-                LEFT OUTER JOIN RollerTable ON OrderDetail.RollerTableID = RollerTable.ID
-            WHERE
-                ([Order].order_id IS NOT NULL)
-                AND (OrderStatus.IsOrder = '1')
-                AND (OrderStatus.IsQuotation = '0')
-                AND (OrderStatus.id NOT LIKE '7')
-                AND (BlindType.id <> '382')
-        ";
-
         // if a blind_id present add additional condition to only load
         // data after this specified blind_id
         if ($latestSerialId && !$loadAll) {
-            $query .= "\t AND (SerialDetailLine.id > {$latestSerialId})";
+            $query .= "\t AND (sdl.id > {$latestSerialId})";
         }
 
         // execute the query
@@ -216,19 +180,15 @@ class PopulateOrdersFromBlindData extends Command
             return false;
         }
 
-        $order['blind_id'] = $sageOrder['BlindId'];
+        $order['serial_id'] = $sageOrder['SerialID'];
         $order['order_no'] = $sageOrder['OrderNo'];
         $order['customer'] = $sageOrder['Customer'];
-        $order['customer_order_no'] = $sageOrder['CustOrdNo'];
-        $order['quantity'] = $sageOrder['Quantity'];
-        $order['blind_type'] = $sageOrder['BlindCode'] ?? '';
-        $order['blind_status'] = $sageOrder['BlindStatus'] ?? '';
+        $order['customer_ref'] = $sageOrder['CustRef'];
+        $order['customer_order_no'] = $sageOrder['CustNo'];
+        $order['product_type'] = $sageOrder['ProductType'];
+        $order['blind_type'] = $sageOrder['ProductCode'];
+        $order['ordered_at'] = $sageOrder['Ordered'];
         $order['order_entered_by'] = $sageOrder['OrderEnteredBy'];
-        $order['despatched_at'] = !empty($sageOrder['DespatchDate']) ? Carbon::parse($sageOrder['DespatchDate'])->format('Y-m-d H:i:s') : null;
-        $order['ordered_at'] = !empty($sageOrder['Ordered']) ? Carbon::parse($sageOrder['Ordered'])->format('Y-m-d H:i:s') : null;
-        $order['category_id'] = $sageOrder['CategoryID'] ?? null;
-        $order['category_name'] = $sageOrder['CategoryName'] ?? '';
-        $order['serial_id'] = $sageOrder['SerialID'];
         $order['created_at'] = now('UTC')->format('Y-m-d H:i:s');
 
         return $order;
---
+++ database/migrations/2021_08_23_141152_alter_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->string('customer_ref')->nullable()->after('customer_order_no');
+            $table->string('product_type')->nullable()->after('category_name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropColumn('customer_ref');
+            $table->dropColumn('product_type');
+        });
+    }
+}
